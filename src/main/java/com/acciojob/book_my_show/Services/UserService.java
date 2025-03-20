0b1bfc014946d08e7742e154fa3b6eb784f61f2f
Lokeshlk354 <lokeshlk354@gmail.com>
2025-03-20 17:59:38 +0530

    Swagger and Email Integration

--- src/main/java/com/acciojob/book_my_show/Services/UserService.java
+++ src/main/java/com/acciojob/book_my_show/Services/UserService.java
@@ -4,6 +4,8 @@ import com.acciojob.book_my_show.Models.User;
 import com.acciojob.book_my_show.Repositories.UserRepository;
 import com.acciojob.book_my_show.Requests.AddUserRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +13,9 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
 
 
     public String addUser(AddUserRequest userRequest) {
@@ -20,6 +25,19 @@ public class UserService {
                 .name(userRequest.getName())
                 .mobileNo(userRequest.getMobileNo())
                 .build();
+
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        mailMessage.setTo(userRequest.getEmailId());
+        mailMessage.setFrom("lohilohilohith999@gmail.com");
+        mailMessage.setSubject("Welcome to Book My Show Application !!");
+
+        String body = "Hi "+userRequest.getName()+" !"+
+                "Welcome to Book My Show Application, Enjoy WELCOME10 to get 10% off on tickets";
+
+        mailMessage.setText(body);
+        javaMailSender.send(mailMessage);
 
         user = userRepository.save(user);
         return "User has been saved to the DB with userId "+user.getUserId();
